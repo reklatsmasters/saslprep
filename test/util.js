3efe10748a1fcee70c9f29a39f95bd0c2847e410
Luiz Américo <luizamericop@gmail.com>
2019-05-06 22:39:52 -0300

    Add inRange utility function

--- test/util.js
+++ test/util.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const { setFlagsFromString } = require('v8');
-const { range } = require('../lib/util');
+const { range, inRange } = require('../lib/util');
 
 // 984 by default.
 setFlagsFromString('--stack_size=500');
@@ -14,3 +14,44 @@ test('should work', () => {
 test('should work for large ranges', () => {
   expect(() => range(1, 1e6)).not.toThrow();
 });
+
+describe('inRange', () => {
+  const rangeMap = [
+    10, 10,
+    12, 20,
+    22, 22,
+    30, 40,
+    50, 55,
+    57, 57
+  ]
+
+  test('should return true for numbers in range map', () => {
+    expect(inRange(10, rangeMap)).toBeTruthy()
+    expect(inRange(12, rangeMap)).toBeTruthy()
+    expect(inRange(15, rangeMap)).toBeTruthy()
+    expect(inRange(20, rangeMap)).toBeTruthy()
+    expect(inRange(22, rangeMap)).toBeTruthy()
+    expect(inRange(30, rangeMap)).toBeTruthy()
+    expect(inRange(31, rangeMap)).toBeTruthy()
+    expect(inRange(35, rangeMap)).toBeTruthy()
+    expect(inRange(39, rangeMap)).toBeTruthy()
+    expect(inRange(40, rangeMap)).toBeTruthy()
+    expect(inRange(52, rangeMap)).toBeTruthy()
+    expect(inRange(57, rangeMap)).toBeTruthy()
+  })
+
+  test('should return false for numbers in range map', () => {
+    expect(inRange(1, rangeMap)).toBeFalsy()
+    expect(inRange(9, rangeMap)).toBeFalsy()
+    expect(inRange(11, rangeMap)).toBeFalsy()
+    expect(inRange(21, rangeMap)).toBeFalsy()
+    expect(inRange(23, rangeMap)).toBeFalsy()
+    expect(inRange(25, rangeMap)).toBeFalsy()
+    expect(inRange(29, rangeMap)).toBeFalsy()
+    expect(inRange(41, rangeMap)).toBeFalsy()
+    expect(inRange(44, rangeMap)).toBeFalsy()
+    expect(inRange(49, rangeMap)).toBeFalsy()
+    expect(inRange(56, rangeMap)).toBeFalsy()
+    expect(inRange(58, rangeMap)).toBeFalsy()
+  })
+})
