f0bedf0991ca634812ac0e53f3e575acef83cb57
Luiz Américo <luizamericop@gmail.com>
2019-05-13 20:28:06 -0300

    Remove unused code

--- test/util.js
+++ test/util.js
@@ -1,19 +1,10 @@
 'use strict';
 
 const { setFlagsFromString } = require('v8');
-const { range, inRange } = require('../lib/util');
+const { inRange } = require('../lib/util');
 
 // 984 by default.
 setFlagsFromString('--stack_size=500');
-
-test('should work', () => {
-  const list = range(1, 3);
-  expect(list).toEqual([1, 2, 3]);
-});
-
-test('should work for large ranges', () => {
-  expect(() => range(1, 1e6)).not.toThrow();
-});
 
 describe('inRange', () => {
   const rangeMap = [10, 10, 12, 20, 22, 22, 30, 40, 50, 55, 57, 57];
